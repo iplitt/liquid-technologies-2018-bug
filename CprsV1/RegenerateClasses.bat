a6a9c1882b51dfeae77336c5fed17960d0bef552
Irwin Plitt <iplitt@alum.mit.edu>
2019-02-20 16:03:10 -0800

    Document the bug.

---
+++ CprsV1/RegenerateClasses.bat
@@ -0,0 +1,2 @@
+﻿
+"C:\Program Files\Liquid Technologies\Liquid Studio 2018\XmlDataBinder16\LXDB16.exe" /NG /V /T XSD /F "Schemas\PhysicalReleaseSchedule_V1.0.xsd" /L C# /TP Standard20 /X ".." /O "CprsV1" /Col PLUR /NP /N "CprsV1"
